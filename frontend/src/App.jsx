7bab48ccfdabc0f54e28f14de24a1067e43d1633
louis.give@utt.fr <louis.give@utt.fr>
2024-11-08 16:19:29 +0100

    chargement dynamique

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import logo from './assets/logosansfondcrop.png'
+import logo from './assets/logo_light.png'
 import ListeVideo from './ListeVideo'
 import Video from './Video'
 import './App.css'
--- frontend/src/ListeVideo.jsx
+++ frontend/src/ListeVideo.jsx
@@ -1,16 +1,21 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
-import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
-import 'dayjs/locale/fr'
-import data from './assets/sample_data.json'
-
-dayjs.extend(relativeTime)
-dayjs.locale('fr')
 
 function ListeVideo() {
-  const videosByRow = Object.values(
-    Object.groupBy(data.videos, (x, i) => Math.floor(i/3))
-  )
+  const [videosByRow, setVideosByRow] = useState([])
+
+  useEffect(() => {
+    fetch('/src/assets/sample_data.json')
+      .then(x => x.json())
+      .then(data => {
+        setVideosByRow(
+          Object.values(
+            Object.groupBy(data.videos, (x, i) => Math.floor(i/3))
+          )
+        )
+      })
+  }, [])
+
   return (
     <main className="container">
       {videosByRow.map((x, i) =>
